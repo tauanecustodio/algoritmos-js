f5cfaff986a6e8fd71fcd41d590e58422a518370
Tauane.Custodio <tauanesantoscustodio@gmail.com>
2024-05-28 14:17:00 -0300

    update exercises

--- exercicios/013/script-013.js
+++ exercicios/013/script-013.js
@@ -5,5 +5,5 @@ document.querySelector('button[type="submit"]').addEventListener('click', functi
     const increase = wage * (15 / 100);
     const newWage = wage + increase;
 
-    document.getElementById('text-area').innerHTML = `Seu aumento será de R$${increase.toFixed(2).replace('.',',')} e seu novo salário será R$${newWage.toFixed(2).replace('.',',')}`;
+    document.getElementById('text-area').innerHTML = `Seu aumento será de R$${increase.toFixed(2).replace('.',',')} e seu salário passará a ser de R$${newWage.toFixed(2).replace('.',',')}`;
 })
--- exercicios/014/script-014.js
+++ exercicios/014/script-014.js
@@ -4,9 +4,6 @@ document.querySelector('button[type="submit"]').addEventListener('click', functi
     const kmValue = parseFloat(document.getElementById('km-value').value);
     const daysValue = parseFloat(document.getElementById('days-value').value);
 
-    console.log (kmValue);
-    console.log (daysValue);
-
     const totalPrice = (kmValue * 0.20) + (daysValue * 90);
 
     document.getElementById('text-area').innerHTML = `O total a pagar é R$${totalPrice.toFixed(2).replace('.',',')}`;
